30be16ec8e200652862ad94ea9d888fad768f4fd
TheHelloWorld <hundunhou@126.com>
2017-12-20 21:28:36 +0800

    提交游戏页面

---
+++ src/main/webapp/js/game/load.js
@@ -0,0 +1,147 @@
+var hc=0, wc=0, di_cha;//屏幕高 阴影最大最小值差
+
+var zhuazi_size = {"up":[], "left":[], "right":[], "di":[] }; //爪子4个部件 长宽左高初始值
+var wawa_init_position = {'2':[], '5':{0:[0.18,0.4,0.63,0.37,0.6],1:[90,92],2:[91,91,91,93,93]}, '8':[] };//娃娃位置初始值
+var wawa_init_size = {'2':[], '5':[185,244], '8':[] };
+var max = {"up":[], "left":[], "right":[], "di":[] }; //爪子4部分 极限位置
+var obj = {"up":"","left":"","right":"","di":"","wawa":"","wawadi":""}; //对象
+var gift_position = []; //所有礼物四边初始位置
+var current_size = {"up":[], "left":[], "right":[], "di":[] };
+
+var flag_status = {"move":true,"get":true};
+var wawa_num = 5;
+function load(){
+	hc = $(window).height();
+	wc = $(window).width();
+	//初始化屏幕大小
+	$(".game-middle").css({'height': hc});
+	$(".game-top").css({'height': hc*0.05});
+	$(".game-bottom").css({'height': hc*0.3});
+	$(".zzc").css({'height': hc*0.5});
+	$(".resultMsg").css('height',hc);
+	$(".successPerson").css({'height': hc*0.9,'margin-top':hc*0.1});
+	$(".broadcastSuccess").css('height',hc*0.1);
+	$(".broadcastSuccessBack").css('height',hc*0.1);
+	
+	//初始化要用到的爪子对象
+	obj['up'] = $(".zhuaziup");
+	obj['left'] = $(".zhuazileft");
+	obj['right'] = $(".zhuaziright");
+	obj['di'] = $(".zhuazidi");
+	
+	//获取爪子 3个部件的宽高
+	zhuazi_size['up']['width'] = $(".zhuaziup").css('width');
+	zhuazi_size['up']['height'] = $(".zhuaziup").css('height');
+	
+	zhuazi_size['left']['width'] = $(".zhuazileft").css('width');
+	zhuazi_size['left']['height'] = $(".zhuazileft").css('height');
+	
+	zhuazi_size['right']['width'] = $(".zhuaziright").css('width');
+	zhuazi_size['right']['height'] = $(".zhuaziright").css('height');
+	
+	current_size['up']['width'] = $(".zhuaziup").css('width');
+	current_size['up']['height'] = $(".zhuaziup").css('height');
+	
+	current_size['left']['width'] = $(".zhuazileft").css('width');
+	current_size['left']['height'] = $(".zhuazileft").css('height');
+	
+	current_size['right']['width'] = $(".zhuaziright").css('width');
+	current_size['right']['height'] = $(".zhuaziright").css('height');
+	
+	//获取爪子影子的宽高和初始位置
+	zhuazi_size['di']['top'] = hc*0.61;//605
+	zhuazi_size['di']['left'] = wc*0.17;//125
+	
+	current_size['di']['top'] = hc*0.61;//605
+	current_size['di']['left'] = wc*0.17;//125
+	
+	obj['di'].css('top',zhuazi_size['di']['top']+"px");
+	obj['di'].css('left',zhuazi_size['di']['left']+"px");
+	zhuazi_size['di']['width'] = (wc * 0.2)+"px";
+	current_size['di']['width'] = (wc * 0.2)+"px";
+	obj['di'].css('width',zhuazi_size['di']['width']);
+	zhuazi_size['di']['height'] = obj['di'].css('height');
+	current_size['di']['height'] = obj['di'].css('height');
+	di_cha = hc * 0.135;
+	
+	//定义爪子三个部件的初始位置
+	zhuazi_size['up']['left'] = wc*0.17;
+	zhuazi_size['left']['left'] = wc*0.16;
+	zhuazi_size['right']['left'] = wc*0.33;
+	//获取初始top值
+	zhuazi_size['up']['top'] = parseFloat(obj['up'].css('top'));
+	zhuazi_size['left']['top'] = parseFloat(obj['left'].css('top'));
+	zhuazi_size['right']['top'] = parseFloat(obj['right'].css('top'));
+	
+	current_size['up']['left'] = wc*0.17;
+	current_size['left']['left'] = wc*0.16;
+	current_size['right']['left'] = wc*0.33;
+	//获取初始top值
+	current_size['up']['top'] = parseFloat(obj['up'].css('top'));
+	current_size['left']['top'] = parseFloat(obj['left'].css('top'));
+	current_size['right']['top'] = parseFloat(obj['right'].css('top'));
+	//设置初始left值
+	obj['up'].css({'left': zhuazi_size['up']['left']+"px"});
+	obj['left'].css({'left': zhuazi_size['left']['left']+"px"});
+	obj['right'].css({'left': zhuazi_size['right']['left']+"px"});
+	
+	max['up']['left'] = wc*0.765;
+	max['left']['left'] = wc*0.71;
+	max['right']['left'] = wc*0.82;
+	max['right']['di'] = wc*0.685
+
+	var wawa_width = wawa_init_size[wawa_num][0];
+	var wawa_height = wawa_init_size[wawa_num][1];
+	//接下来布置娃娃位置大小
+	var str = "";
+	for(var key in wawa_init_position[wawa_num][0]){
+		var top,left,top2,index;
+		if(key < 3){ 
+			top = hc*0.37+"px";
+			top2 = hc*0.37+wawa_init_size[wawa_num][1]-10+"px";
+			index = wawa_init_position[wawa_num][1][0];
+		}else{ 
+			top = hc*0.475+"px";
+			top2 = hc*0.475+wawa_init_size[wawa_num][1]-10+"px";
+			index = wawa_init_position[wawa_num][1][1]
+		}
+		left = wc * wawa_init_position[wawa_num][0][key]+"px";
+		str += '<div id="liwu'+key+'" style="position:absolute;z-index:'+index+';top:'+top+';left:'+left+'"><img src="'+toyRoomImg+'" class="wawa" style="z-index:'+index+'"/></div><div id="wawaback'+key+'" style="top:'+top2+';left:'+left+';width:185px;height:20px;background-repeat:no-repeat; background-size:100% 100%;-moz-background-size:100% 100%;position:absolute;z-index:'+index+';text-align:center" class="wawaback"><img src="image1/wawadi.png" class="wawaback" style="width:90%;height:30px;z-index:'+index+'"/></div>';
+	}
+	$(".loadgift").append(str);
+	//获取每个娃娃的上下左右四个位置
+	for(var key in wawa_init_position[wawa_num][0]){
+		gift_position[key] = [];
+		gift_position[key]['left'] = wc * wawa_init_position[wawa_num][0][key] + (wawa_width/3);
+		gift_position[key]['right'] =  wc * wawa_init_position[wawa_num][0][key] + (wawa_width/3 *2);
+		if(key < 3){ top = hc*0.37; }else{ top = hc*0.475; }
+		gift_position[key]['top'] = top + wawa_height;
+		gift_position[key]['down'] = top + wawa_height+20;
+	}
+	$(".loadinggif").remove();
+	$(".loading").remove();
+}
+
+window.ontouchstart = function(e) { e.preventDefault(); };
+
+function showDiv(type){
+	if(type == 'list'){
+		$(".successPersontitleleft").css('border-bottom','3px solid rgb(109, 219, 246)');
+		$(".successPersontitleright").css('border-bottom','0px solid rgb(109, 219, 246)');
+		$(".successList").show();
+		$(".successdetail").hide();
+	}else{
+		$(".successPersontitleleft").css('border-bottom','0px solid rgb(109, 219, 246)');
+		$(".successPersontitleright").css('border-bottom','3px solid rgb(109, 219, 246)');
+		$(".successList").hide();
+		$(".successdetail").show();
+	}
+}
+
+function setBotton(){
+	$(".botton-top").attr({"ontouchstart":"movesmall()", "ontouchend":"stopmove()"});
+	$(".botton-left").attr({"ontouchstart":"moveleft()", "ontouchend":"stopmove()"});
+	$(".botton-bottom").attr({"ontouchstart":"movebig()", "ontouchend":"stopmove()"});
+	$(".botton-right").attr({"ontouchstart":"moveright()", "ontouchend":"stopmove()"});
+	$(".botton-get").attr({"ontouchstart":"catchToy()"});
+}
---
+++ src/main/webapp/js/game/socket.js
@@ -0,0 +1,267 @@
+var wsUri ="ws://45.76.51.107:20002/"; 
+var output;  
+var uId;
+var rId;
+var coin;
+var time1;
+var toyImg;
+var toyNo;
+var toyNowCoin;
+var toyName;
+var toyRoomImg;
+var luckyNum;
+var catchId;
+
+function startWebSocket() { 
+    websocket = new WebSocket(wsUri); 
+    websocket.onopen = function(evt) { 
+        onOpen(evt)
+    }; 
+    websocket.onclose = function(evt) { 
+        onClose(evt) 
+    }; 
+    websocket.onmessage = function(evt) { 
+        onMessage(evt) 
+    }; 
+    websocket.onerror = function(evt) { 
+        onError(evt) 
+    };
+}  
+startWebSocket()
+function onOpen(evt) {
+	getInfo();
+}  
+
+function onClose(evt) {
+	
+}  
+
+function onMessage(evt) {
+	data = eval('('+evt.data+')');
+	if(data.type == 'init'){
+		setInit(data);
+	}else if(data.type == 'successList'){
+		successList(data);
+	}else if(data.type == 'start'){
+		startGameStatus(data);
+	}else if(data.type == 'catchStatus'){
+		catchStatus(data);
+	}else if(data.type == 'broadcast'){
+		broadcast(data);
+	}else if(data.type == 'newSuccess'){
+		newSuccess();
+	}else if(data.type == 'refreshCurNum'){
+		refreshCurNum(data);
+	}
+}
+
+function refreshCurNum(data){
+	$(".curPlayer").html('在线人数:'+data.count);
+}
+
+function timer(intDiff){
+	time1 = window.setInterval(function(){
+		if((intDiff) == 0){
+			window.clearInterval(time1);
+		}else{
+			var s = intDiff;
+			if(s < 9){
+				s_str = 0+s.toString();
+			}else{
+				s_str = s;
+			}
+			$(".djs").html(s_str);
+			intDiff--;
+		}
+	},1000)
+
+}
+
+function onError(evt) { 
+    console.log(evt);
+}
+
+function closeSocket() {
+    websocket.close();
+}
+
+function doSend(message) {
+    websocket.send(message); 
+}
+
+function setInit(data){
+	current_id = -1;
+	setBotton();
+	toyImg = data.result.toyRoomImg;
+	toyNo = data.result.toyNo;
+	toyNowCoin = data.result.toyNowCoin;
+	toyName = data.result.toyName;
+	toyRoomImg = data.result.toyRoomImg;
+	luckyNum = data.result.userGameRoomLuckyNumB;
+	var str = '<div style="text-align:center;font-size:50px;margin-top:5%;font-weight:bold">'+toyName+'</div>';
+	for(var key in data.result.toyDesc){
+		str +='<div style="text-align:center;margin-top:5%;"><img src="'+data.result.toyDesc[key]+'" /></div>';
+	}
+	$(".successdetaildiv").html(str);
+	$(".showOnceCoinCur").html(toyNowCoin+"/局");
+	var weight = (luckyNum/100 *34);
+	$(".luckyNum").css('width',weight+'%');
+	$(".curPlayer").html("在线人数："+data.result.curPlayer);
+	var userCoin = sessionStorage["toiletCatUserCoin"];
+	$(".showCurCoin").html(userCoin);
+	load();
+}
+
+function catchToy(){
+	clearInterval(startDjsTimer);
+	uId = getQueryString('userNo');
+	rId = getQueryString('gameRoomNo');
+	userName = sessionStorage["toiletCatUserName"];
+	userImg = sessionStorage["toiletCatUserImg"];
+	removebroadcast();
+	if(current_id > -1){
+		status = 1; 
+	}else{
+		status = 0;
+	}
+	var sign = "catch&"+uId+"&"+rId;
+	sign = hex_md5(sign);
+	var json = '{"type":"catch","uId":'+uId+',"rId":"'+rId+'","sign":"'+sign+'","status":"'+status+'","uName":"'+userName+'","tName":"'+toyName+'","userImg":"'+userImg+'"}';
+	doSend(json);
+}
+
+function getInfo(){
+	uId = getQueryString('userNo');
+	rId = getQueryString('gameRoomNo');
+	var json = '{"type":"init","uId":'+uId+',"rId":"'+rId+'"}';
+	doSend(json);
+}
+
+function catchStatus(data){
+	if(data.result.catch_result == 'catch success'){
+		status = 1;
+	}else{
+		status = 0;
+		luckyNum = luckyNum+data.result.addNum;
+	}
+	getprize();
+}
+
+function startGame(){
+	var sign = 'start&'+uId+"&"+toyNowCoin+"&"+rId;
+	sign = hex_md5(sign);
+	var json = '{"type":"start","uId":'+uId+',"rId":"'+rId+'","coin":"'+toyNowCoin+'","sign":"'+sign+'"}';
+	doSend(json);
+}
+
+function successList(data){
+	var str = "";
+	for(var key in data.result){
+		str +='<div class="successListdiv">';
+		str +='  <div class="successListimg">';
+		str +='    <img src="'+data.result[key].userImg+'" />';
+		str +='  </div>';
+		str +='  <div class="successListname">';
+		str +='  <div>'+data.result[key].userName+'</div>';
+		str +='  <div>'+data.result[key].catchTime+'</div>';
+		str +='  </div>';
+		//str +='  <div class="successListread" style="background-image:url(/images/botton.jpg)">';
+		//str +='  Ta的战利品';
+		//str +='  </div>';
+		str +='  <div style="clear:both"></div>';
+		str +='  </div>';
+	}
+	$(".successList").html(str);
+}
+
+function showDiv(type){
+	if(type == 'list'){
+		$(".successPersontitleleft").css('border-bottom','3px solid rgb(109, 219, 246)');
+		$(".successPersontitleright").css('border-bottom','0px solid rgb(109, 219, 246)');
+		$(".successList").show();
+		$(".successdetail").hide();
+	}else{
+		$(".successPersontitleleft").css('border-bottom','0px solid rgb(109, 219, 246)');
+		$(".successPersontitleright").css('border-bottom','3px solid rgb(109, 219, 246)');
+		$(".successList").hide();
+		$(".successdetail").show();
+	}
+}
+var myStr; 
+var successArr = [];
+function broadcast(data){
+	if(data.status == 'fail'){
+		var top = data.top;
+		catchId = data.catchId;
+		if(data.isMe != 1){
+			var str = '<div class="'+data.catchId+'_1" style="position:absolute;top:'+top+'%;background:white;opacity:0.5;width:100%;height:40%;"></div><div class="'+data.catchId+'"  style="position:absolute;left:100%;top:'+top+'%;font-size:50px;width:150%;height:40%;">'+data.str+'</div>';
+			$(".broadcastFail").append(str);
+			$("."+data.catchId).animate({"left":"-200%"},8000);
+			setTimeout(removebroadcast,5000);
+		}else{
+			myStr = '<div class="'+data.catchId+'_1" style="position:absolute;top:'+top+'%;background:white;opacity:0.5;width:100%;height:40%;"></div><div class="'+data.catchId+'"  style="position:absolute;left:100%;top:'+top+'%;font-size:50px;width:150%;height:40%;">'+data.str+'</div>';
+		}
+	}else{
+		if(data.isMe != 1){
+			$(".broadcastSuccesswords").html(data.str);
+			$(".broadcastSuccess").show();
+			setTimeout(hidebroadcast,2000);
+		}else{
+			$(".broadcastSuccesswords").html(data.str);
+		}
+		var str = "";
+		str +='<div class="successListdiv">';
+		str +='  <div class="successListimg">';
+		str +='    <img src="'+data.userImg+'" />';
+		str +='  </div>';
+		str +='  <div class="successListname">';
+		str +='  <div>'+data.userName+'</div>';
+		str +='  <div>'+data.catchTime+'</div>';
+		str +='  </div>';
+		str +='  <div style="clear:both"></div>';
+		str +='  </div>';
+		str +=$(".successList").html();
+		$(".successList").html(str);
+	}
+	
+}
+function removebroadcast(){
+	$("."+catchId).remove();
+	$("."+catchId+"_1").remove();
+}
+
+function hidebroadcast(){
+	$(".broadcastSuccess").hide();
+}
+var startDjsTimer;
+function startGameStatus(data){
+	if(data.result.result == "success"){
+		var startdjs = 30;
+		setBotton();
+		$("#startButton").hide();
+		$("#moveButton").show();
+		var coin = parseInt($(".showCurCoin").html());
+		$(".showCurCoin").html(coin-toyNowCoin);
+		$(".successPerson").hide();
+		
+		startDjsTimer = window.setInterval(function(){
+			if(startdjs == 0){
+				clearInterval(startDjsTimer);
+				catchToy();
+			}else{
+				startdjs--;
+				$("#startdjs").html(startdjs);
+			}
+		},1000)
+	}else{
+		chongzhi();
+	}
+}
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null)
+        return unescape(r[2]);
+    return null;
+}
