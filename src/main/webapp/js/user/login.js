7818cd674bcb4d4eba7ded6518e28833ede57409
liuzikun <hundunhou@126.com>
2018-01-19 16:02:30 +0800

    修改登录页

--- src/main/webapp/js/user/login.js
+++ src/main/webapp/js/user/login.js
@@ -10,6 +10,148 @@ var type = "";
 
 var from = "";
 
+// 图片验证码
+var verifyCode;
+
+!(function(window, document) {
+    var size = 5;//设置验证码长度
+    function GVerify(options) { //创建一个图形验证码对象，接收options对象为参数
+        this.options = { //默认options参数值
+            id: "", //容器Id
+            canvasId: "verifyCanvas", //canvas的ID
+            width: "100", //默认canvas宽度
+            height: "30", //默认canvas高度
+            type: "blend", //图形验证码默认类型blend:数字字母混合类型、number:纯数字、letter:纯字母
+            code: "",
+        }
+        if(Object.prototype.toString.call(options) == "[object Object]"){//判断传入参数类型
+            for(var i in options) { //根据传入的参数，修改默认参数值
+                this.options[i] = options[i];
+            }
+        }else{
+            this.options.id = options;
+        }
+
+        this.options.numArr = "0,1,2,3,4,5,6,7,8,9".split(",");
+        this.options.letterArr = getAllLetter();
+
+        this._init();
+        this.refresh();
+    }
+
+    GVerify.prototype = {
+        /**版本号**/
+        version: '1.0.0',
+
+        /**初始化方法**/
+        _init: function() {
+            var con = document.getElementById(this.options.id);
+            var canvas = document.createElement("canvas");
+            this.options.width = con.offsetWidth > 0 ? con.offsetWidth : "100";
+            this.options.height = con.offsetHeight > 0 ? con.offsetHeight : "30";
+            canvas.id = this.options.canvasId;
+            canvas.width = this.options.width;
+            canvas.height = this.options.height;
+            canvas.style.cursor = "pointer";
+            canvas.innerHTML = "您的浏览器版本不支持canvas";
+            con.appendChild(canvas);
+            var parent = this;
+            canvas.onclick = function(){
+                parent.refresh();
+            }
+        },
+
+        /**生成验证码**/
+        refresh: function() {
+            this.options.code = "";
+            var canvas = document.getElementById(this.options.canvasId);
+            if(canvas.getContext) {
+                var ctx = canvas.getContext('2d');
+            }else{
+                return;
+            }
+
+            ctx.textBaseline = "middle";
+
+            ctx.fillStyle = randomColor(180, 240);
+            ctx.fillRect(0, 0, this.options.width, this.options.height);
+
+            if(this.options.type == "blend") { //判断验证码类型
+                var txtArr = this.options.numArr.concat(this.options.letterArr);
+            } else if(this.options.type == "number") {
+                var txtArr = this.options.numArr;
+            } else {
+                var txtArr = this.options.letterArr;
+            }
+
+            for(var i = 1; i <=size; i++) {
+                var txt = txtArr[randomNum(0, txtArr.length)];
+                this.options.code += txt;
+                ctx.font = randomNum(this.options.height/2, this.options.height) + 'px SimHei'; //随机生成字体大小
+                ctx.fillStyle = randomColor(50, 160); //随机生成字体颜色
+                ctx.shadowOffsetX = randomNum(-3, 3);
+                ctx.shadowOffsetY = randomNum(-3, 3);
+                ctx.shadowBlur = randomNum(-3, 3);
+                ctx.shadowColor = "rgba(0, 0, 0, 0.3)";
+                var x = this.options.width / (size+1) * i;
+                var y = this.options.height / 2;
+                var deg = randomNum(-30, 30);
+                /**设置旋转角度和坐标原点**/
+                ctx.translate(x, y);
+                ctx.rotate(deg * Math.PI / 180);
+                ctx.fillText(txt, 0, 0);
+                /**恢复旋转角度和坐标原点**/
+                ctx.rotate(-deg * Math.PI / 180);
+                ctx.translate(-x, -y);
+            }
+            /**绘制干扰线**/
+            for(var i = 0; i < 4; i++) {
+                ctx.strokeStyle = randomColor(40, 180);
+                ctx.beginPath();
+                ctx.moveTo(randomNum(0, this.options.width), randomNum(0, this.options.height));
+                ctx.lineTo(randomNum(0, this.options.width), randomNum(0, this.options.height));
+                ctx.stroke();
+            }
+            /**绘制干扰点**/
+            for(var i = 0; i < this.options.width/4; i++) {
+                ctx.fillStyle = randomColor(0, 255);
+                ctx.beginPath();
+                ctx.arc(randomNum(0, this.options.width), randomNum(0, this.options.height), 1, 0, 2 * Math.PI);
+                ctx.fill();
+            }
+        },
+
+        /**验证验证码**/
+        validate: function(code){
+            var code = code.toLowerCase();
+            var v_code = this.options.code.toLowerCase();
+            if(code == v_code){
+                return true;
+            }else{
+                this.refresh();
+                return false;
+            }
+        }
+    };
+    /**生成字母数组**/
+    function getAllLetter() {
+        var letterStr = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+        return letterStr.split(",");
+    }
+    /**生成一个随机数**/
+    function randomNum(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    /**生成一个随机色**/
+    function randomColor(min, max) {
+        var r = randomNum(min, max);
+        var g = randomNum(min, max);
+        var b = randomNum(min, max);
+        return "rgb(" + r + "," + g + "," + b + ")";
+    }
+    window.GVerify = GVerify;
+})(window, document);
+
 $(function() {
 
     from = getQueryString("from");
@@ -54,34 +196,17 @@ $(function() {
         'width',wc*0.6
     );
 
-    if(checkType == "checkCode") {
-        // 产生验证码
-        createCode();
-        $("#checkTextDiv").hide();
-    } else {
+    if(checkType == "checkText") {
         $("#checkCodeDiv").hide();
         $("#checkTextDiv").show();
+    } else {
+
+        // 产生验证码
+        verifyCode = new GVerify("v_container");
+        $("#checkTextDiv").hide();
+
     }
 });
-
-// 产生验证码
-function createCode() {
-    code = "";
-    // 验证码的长度
-    var codeLength = 4;
-    // 随机数
-    var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-
-    // 循环操作
-    for(var i = 0; i < codeLength; i++) {
-        // 取得随机数的索引（0~35）
-        var index = Math.floor(Math.random() * 36);
-        // 根据索引取得随机数加到code上
-        code += random[index];
-    }
-    $("#checkCode").val(code);
-}
 
 // 点击登陆按钮方法
 function login() {
@@ -96,48 +221,51 @@ function login() {
         return;
     }
 
-    if(checkType == "checkCode") {
-        validate();
-    } else {
+    if(checkType == "checkText") {
         loginOrRegister();
+    } else {
+        validate();
     }
 }
 
 // 校验验证码
 function validate() {
-    // 获取输入框内验证码并转化为大写
-    var inputCode = $("#checkCodeText").val().toUpperCase();
-
     // 若输入的验证码为空
     if(!isNotNull("checkCodeText")) {
         toiletCatMsg("请输入验证码", null);
         return;
     }
 
+    // 获取输入框内验证码
+    var inputCode = $("#checkCodeText").val();
+
+    var res = verifyCode.validate(inputCode);
+
     // 若输入的验证码与产生的验证码不一致时
-    if(inputCode != code) {
+    if(!res) {
         // 则弹出验证码输入错误
         toiletCatMsg("验证码输入错误", null);
         // 刷新验证码
-        createCode();
+        verifyCode.refresh();
         // 清空文本框
         $("#checkCodeText").val("");
         return;
-    } else {
-        checkType = "checkText";
-
-        var url = window.location.href;
-
-        url = url.split("checkType=")[0] + "checkType=" + checkType;
-
-        window.history.pushState({},0,url);
-
-        $("#checkCodeDiv").hide();
-        $("#checkTextDiv").show();
     }
+
+    checkType = "checkText";
+
+    var url = window.location.href;
+
+    url = url.split("checkType=")[0] + "checkType=" + checkType;
+
+    window.history.pushState({},0,url);
+
+    $("#checkCodeDiv").hide();
+    $("#checkTextDiv").show();
+
 }
 
-// 法送短信验证码
+// 发送短信验证码
 function sendTextCode(o) {
 
     if(!isNotNull("mobile")) {
@@ -249,22 +377,22 @@ function loginOrRegister() {
             // 游戏主页
             if(from == "gameIndex") {
 
-                url = "/toiletCat/gameRoom/gameRoom.html?nowType=login&userNo="+user["userNo"];
+                url = "/toiletCat/gameRoom/gameRoom.html?nowType=login&userNo=" + user["userNo"];
             // 用户主页
             } else if(from == "userIndex") {
 
-                url = "/toiletCat/user/userIndex.html?type="+type+"&userNo="+user["userNo"];
+                url = "/toiletCat/user/userIndex.html?type=" + type + "&userNo=" + user["userNo"];
             // 充值页面
             } else if(from == "recharge") {
-                url = "/toiletCat/gameRoom/gameRoom.html?type="+type+"&userNo="+user["userNo"];
+                url = "/toiletCat/gameRoom/gameRoom.html?type=" + type + "&userNo=" + user["userNo"];
             // 战利品页
             } else if(from == "userToy") {
 
-                url = "/toiletCat/userToy/userToy.html?type="+type+"&userNo="+user["userNo"];
+                url = "/toiletCat/userToy/userToy.html?type=" + type + "&userNo=" + user["userNo"];
             // 机器主页
             } else {
 
-                url = "/toiletCat/machineRoom/machineRoom.html?nowType=login&userNo="+user["userNo"];
+                url = "/toiletCat/machineRoom/machineRoom.html?nowType=login&userNo=" + user["userNo"];
 
             }
 
