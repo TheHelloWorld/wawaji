5316ff7605be5ef16de6d78f0cf17e96f5d3f88d
TheHelloWorld <hundunhou@126.com>
2018-01-22 00:46:12 +0800

    修改注释

--- src/main/java/com/toiletCat/constants/BaseConstant.java
+++ src/main/java/com/toiletCat/constants/BaseConstant.java
@@ -215,7 +215,7 @@ public class BaseConstant {
 	public static final String VIP_USER_GAME_ROOM_ADD_NUM = "vip_user_game_room_add_num";
 
 	/**
-	 * 充值游戏房间幸运值下限(上限为下限+10以内的一个数)
+	 * 重置游戏房间幸运值下限(上限为下限+10以内的一个数)
 	 */
 	public static final String RESET_GAME_ROOM_LUCKY_NUM = "reset_game_room_lucky_num";
 
