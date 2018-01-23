82a8715698f74c33a67995d6b983ae8aec0fa132
liuzikun <hundunhou@126.com>
2018-01-23 12:04:54 +0800

    提交修改

--- src/main/webapp/js/user/share.js
+++ src/main/webapp/js/user/share.js
@@ -6,7 +6,8 @@ $.ajax({
     url:"/toiletCat/api/weChat/getWxShareInfo.action",
     type:"post",
     data:{"url":currurl},
-    success:function(data){
+    success:function(data) {
+        data = eval('('+data+')');
         wx.config({
             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: data.appId, // 必填，公众号的唯一标识
