789c3a4de094608912996454187573bc438127b8
TheHelloWorld <hundunhou@126.com>
2018-01-27 20:50:34 +0800

    修改充值

--- src/main/webapp/js/recharge/recharge.js
+++ src/main/webapp/js/recharge/recharge.js
@@ -8,6 +8,9 @@ function recharge() {
         url:"/toiletCat/api/moneyForCoin/getAllCanSeeMoneyForCoin.action",
         type:"POST",
         async:false,
+        data:{
+            userNo:sessionStorage["toiletCatUserNo"]
+        },
         success:function(data) {
 
             if(typeof(data) == "string"){
@@ -25,7 +28,7 @@ function recharge() {
             var str = "	<div style='top:"+recharge_hc+"px;' class='recharge'>";
             str += "		<div class='recharge-first-line' >";
             str += "			<div style='float:left;height: 100%'>";
-            str += "				<img style='-webkit-transform:rotate(-90deg);' src='/image/game/returnButton.ico' class='rechargeReturn' onclick='closeRecharge()'/>";
+            str += "				<img style='-webkit-transform:rotate(-90deg);' src='/image/returnButton.ico' class='rechargeReturn' onclick='closeRecharge()'/>";
             str += "			</div>";
             str += "		<div class='recharge-user-coin' >";
             str += "			<div class='recharge-coin' >";
@@ -41,26 +44,45 @@ function recharge() {
             str += "	<div style='float: left;' >";
 
             for(var i = 0; i<list.length; i++) {
+                var money_class = "recharge-money";
 
+                var coin = list[i]["coin"];
+                var coinText = list[i]["coinText"];
+                // 判断是否限充
                 if(list[i]["rechargeLimit"] != 0) {
 
+                    if(list[i]["userLimitFlag"] < list[i]["rechargeLimit"]) {
+                        str += "	<div class='recharge-block' onclick=rechargeThis('" + list[i]["money"] + "') >";
+                        str += "	<img class='recharge-limit' src='/image/recharge/limit_recharge.png'>";
+                    } else {
+                        str += "	<div class='recharge-disabled' >";
+                        money_class = "recharge-money-disabled";
+                    }
+                    // 判断是否首充
+                } else if(list[i]["firstFlag"] != 0) {
+                    str += "	<div class='recharge-block' onclick=rechargeThis('" + list[i]["money"] + "') >";
+                    if(list[i]["userFirstFlag"] == "is_first") {
+                        str += "	<img class='recharge-first' src='/image/recharge/first_recharge.png'>";
+                        coin += list[i]["giveCoin"];
+                        coinText += "+" + list[i]["giveCoin"];
+                    }
+                } else {
+                    str += "	<div class='recharge-block' onclick=rechargeThis('" + list[i]["money"] + "') >";
                 }
 
-                if(list[i]["firstFlag"] != 0) {
-
-                }
-
-                str += "	<div class='recharge-block' onclick=rechargeThis('" + list[i]["money"] + "') >";
                 str += "		<div class='recharge-coin' >";
                 str += "			<img src='/image/background/coin-img.png' width=100% height=100% />";
                 str += "		</div>";
                 str += "		<div class='recharge-coin' >";
-                str +=              list[i]["coinText"];
+                str +=              coinText;
                 str += "		</div>";
                 str += "		<div class='recharge-coin-text' >";
-                str +=               list[i]["showText"];
+                str +=               list[i]["showText"].replace("#{coin}",coin);
+                if(list[i]["rechargeLimit"] != 0) {
+                    str += " (" + list[i]["userLimitFlag"] + "/" + list[i]["rechargeLimit"] + ")";
+                }
                 str += "		</div>";
-                str += "		<div class='recharge-money' >";
+                str += "		<div class='" + money_class + "' >";
                 str += "			¥" + list[i]["money"];
                 str += "		</div>";
                 str += "		<div class='clear:both'>";
